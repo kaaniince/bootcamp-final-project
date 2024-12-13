87adcadcfd4f4c660b98a9ec30de142188b55e8f
Kaan Ince <kaaniince@outlook.com>
2024-12-13 15:27:53 +0300

    added productdetails

--- ecommerce-shop/src/pages/ProductDetails.jsx
+++ ecommerce-shop/src/pages/ProductDetails.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ProductContext } from "../contexts/ProductContext";
+import { CartContext } from "../contexts/CartContext";
 
 function ProductDetails() {
-  return <div>Product Details Page</div>;
+  const { id } = useParams();
+  const { products } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
+  const product = products.find((item) => item.id === parseInt(id));
+
+  if (!product) {
+    return (
+      <section className="h-screen flex justify-center items-center">
+        <p className="text-primary text-4xl">Loading...</p>
+      </section>
+    );
+  }
+
+  const { title, price, description, image } = product;
+
+  return (
+    <section className="py-32 pb-12 lg:py-48 h-screen flex items-center">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0 ">
+            <img
+              className="max-w-[200px] lg:max-w-sm"
+              src={image}
+              alt={title}
+            />
+          </div>
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-3xl font-bold mb-2 max-w-[400px] mx-auto lg:mx-0 lg:text-left">
+              {title}
+            </h1>
+            <div className="mb-4 text-xl font-medium">${price}</div>
+            <p className="mb-8">{description}</p>
+            <button
+              onClick={() => addToCart(product, product.id)}
+              className="bg-primary text-white px-6 py-4 rounded-md"
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ProductDetails;
